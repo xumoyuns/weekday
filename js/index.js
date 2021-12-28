df47d1b6da1994d07858779e3721e6cc1e9cbbba
xumoyuns <xumoyunwebcoder@gmail.com>
2021-12-28 08:40:59 +0500

    wekday fineshed

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let elForm = document.querySelector(".form");
+let elFormInput = document.querySelector(".form__input");
+let elFormResult = document.querySelector(".form__result");
+elForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    let day=elFormInput.value;
+switch (day){
+    case "1":
+        elFormResult.textContent="Dushanba";
+        break;
+    case "2":
+        elFormResult.textContent="Seshanba";
+        break;
+    case "3":
+        elFormResult.textContent="Chorshanba";
+        break;
+    case "4":
+        elFormResult.textContent="Payshanba";
+        break;
+    case "5":
+        elFormResult.textContent="Juma";
+        break;
+    case "6":
+        elFormResult.textContent="Shanba";
+        break;
+    case "7":
+        elFormResult.textContent="Yakshanba";
+        break;
+    default:
+        elFormResult.textContent="Iltimo 1-7 gacha bo`lgan raqamlarni kirirting"
+
+}
+
+
+
+
+
+
+
+})
